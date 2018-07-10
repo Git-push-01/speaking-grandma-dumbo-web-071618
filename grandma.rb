8d2cbc29bde9fdc765f4a3aa8c9854535f05e2e7
Hugo Escandon <hescandon6@gmail.com>
2018-07-10 14:40:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,19 +1,7 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(string)
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "Hi!"
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma == "WHAT?"
-    return "NO, NOT SINCE 1938!"
-  else speak_to_grandma == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
-  end
-
+def speak_to_grandma(phrase.uppercase)
+  if phrase == uppercase
+  
 
 
 
