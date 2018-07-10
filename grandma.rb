b8e5bbd75e382c83ea1cb3bedffc79353397a55e
Hugo Escandon <hescandon6@gmail.com>
2018-07-10 14:30:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,24 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma()
-  
+def speak_to_grandma
+  if speak_to_grandma == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif speak_to_grandma == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif speak_to_grandma == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  elsif speak_to_grandma == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  else speak_to_grandma == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+    
+
+
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
